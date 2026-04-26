725930643def1e6afcb4109ac5c7fd958e7b04f9
diogo2806000 <diogo666888@gmail.com>
2026-04-25 23:03:01 -0300

    a

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ejCbHV4fctS/XXH5pZaitU5zOjqUjFs8iTnW4t14jSQ=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"SXAv/qCRfWqm4O6vM7yIbdhSg69MXlt9putSb0o3Yz0=\"}"
