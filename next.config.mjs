229f2f71a8ac510e00a411bd5cb1d14c1052200a
diogo2806000 <diogo666888@gmail.com>
2026-04-25 23:44:19 -0300

    Limpando projeto para o Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Gera o site como arquivos estáticos (HTML/CSS/JS puros)
   output: 'export',
-  images: { unoptimized: true },
-  basePath: '/uni-saude',
-  assetPrefix: '/uni-saude/'
+  
+  // Impede o erro de otimização de imagem no GitHub Pages
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
